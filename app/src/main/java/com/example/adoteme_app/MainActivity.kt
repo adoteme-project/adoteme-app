7975970f3de5346b9b26a291fe8ef8aff81e1e11
CaueGhelfi03 <caue.ghelfi@sptech.school>
2025-02-27 22:39:15 -0300

    feat: GridLayout component

--- app/src/main/java/com/example/adoteme_app/MainActivity.kt
+++ app/src/main/java/com/example/adoteme_app/MainActivity.kt
@@ -1,50 +1,60 @@
-package com.example.adoteme_app
-
 import android.os.Bundle
+import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.adoteme_app.ui.theme.AdotemeappTheme
+import com.example.adoteme_app.ui.components.GridLayout.Animal
+import com.example.adoteme_app.ui.components.GridLayout.GridLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            AdotemeappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Adoteme",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyApp {}
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    Text(
-        text = "O Mobile"
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    MaterialTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.PIXEL_2, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    AdotemeappTheme {
-        Greeting("Adoteme")
+fun DefaultPreview() {
+    MyApp {
+        val animals = listOf(
+            Animal(
+                id = 1,
+                name = "Rex",
+                idade = 3,
+                sexo = "Macho",
+                imageUrl = ""
+            ),
+            Animal(
+                id = 2,
+                name = "Luna",
+                idade = 2,
+                sexo = "Fêmea",
+                imageUrl = "https://www.petz.com.br/blog/wp-content/uploads/2019/03/cachorro-mini-pet-1280x720.jpg"
+            ),
+            // Adicione mais animais aqui...
+        )
+        GridLayout(
+            animals = animals,
+            columns = 2,
+        )
     }
 }
